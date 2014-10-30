a9553d5f93441f990cedeb673815b79515546940
Sandra <sandra.nina.ma@gmail.com>
2014-10-30 16:56:34 +0100

    config test

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=eventjunkie',
     'username' => 'root',
-    'password' => '123',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
