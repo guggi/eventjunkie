76a4eaddf5a220f995b6c820d4bba8c1bd1b4b65
Philipp Hiermann <a1207617@unet.univie.ac.at>
2014-12-13 18:30:44 +0100

    FacebookApi begonnen, zu implementieren

--- models/apis/FacebookApi.php
+++ models/apis/FacebookApi.php
@@ -3,14 +3,23 @@
 namespace app\models\apis;
 
 use Facebook\FacebookRequest;
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
 
 class FacebookApi {
     private $url;
     private $data;
+    private $session;
+    private $graphObject;
 
     function __construct($url) {
         $this->url = $url;
-        //$id = $this->getID();
+        FacebookSession::setDefaultApplication('675866902530372', '5650b9940ca593edf25a6b799308d233');
+        $helper = new FacebookRedirectLoginHelper();
+        $this->session = $helper->getSessionFromRedirect();
+        $request = new FacebookRequest($session, 'GET', '\me');
+        $response = $request->execute();
+        $this->graphObject = $response->getGraphObject();
     }
 
     public function getComments() {
