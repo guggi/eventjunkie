67669ef22c0804f1d51bdf93157c6cebd00313f7
Sandra <sandra.nina.ma@gmail.com>
2015-01-01 23:35:39 +0100

    Java-Script Gallery zu gallery.php hinzugefügt

--- views/event/gallery.php
+++ views/event/gallery.php
@@ -12,6 +12,7 @@ $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id
 $this->params['breadcrumbs'][] = 'Gallery';
 ?>
 
+<link rel="stylesheet" href="css/blueimp-gallery.min.css">
 
 <div class="event-gallery">
 
@@ -20,29 +21,49 @@ $this->params['breadcrumbs'][] = 'Gallery';
         <div class="col-md-12">
 
             <!-- images from linked sites -->
+
+
             <?php
             if (isset($socialmedia['images'])) { ?>
                 <div class="col-md-12">
-                <?php
-                $i = 0;
-                foreach ($socialmedia['images'] as $socialmedia_image) { ?>
-                    <div class="col-md-2">
-                        <a href="<?= $socialmedia_image['original'] ?>">
-                            <img class="thumbnail" src="<?= $socialmedia_image['thumbnail'] ?>" alt="<?= $socialmedia_image['thumbnail'] ?>" />
-                        </a>
-                    </div>
-                    <?php
-                    $i++;
-                    if ($i > 0 && ($i % 6 === 0)) { ?>
-                        </div>
-                        <div class="col-md-12">
-                    <?php }?>
+                    <div id="links">
+                        <?php
+                        foreach ($socialmedia['images'] as $socialmedia_image) { ?>
+                            <div class="col-md-2">
+                                <a href="<?= $socialmedia_image['original'] ?>">
+                                    <img class="thumbnail" src="<?= $socialmedia_image['thumbnail'] ?>" alt="<?= $socialmedia_image['thumbnail'] ?>" />
+                                </a>
+                            </div>
 
-                <?php } ?>
+                        <?php } ?>
+
+                    </div>
                 </div>
             <?php } ?>
 
         </div>
     </div>
 </div>
+
+<!-- The Gallery as lightbox dialog, should be a child element of the document body -->
+<div id="blueimp-gallery" class="blueimp-gallery">
+    <div class="slides"></div>
+    <h3 class="title"></h3>
+    <a class="prev">‹</a>
+    <a class="next">›</a>
+    <a class="close">×</a>
+    <a class="play-pause"></a>
+    <ol class="indicator"></ol>
 </div>
+
+<script src="js/blueimp-gallery.min.js"></script>
+<script>
+    document.getElementById('links').onclick = function (event) {
+        event = event || window.event;
+        var target = event.target || event.srcElement,
+            link = target.src ? target.parentNode : target,
+            options = {index: link, event: event},
+            links = this.getElementsByTagName('a');
+        blueimp.Gallery(links, options);
+    };
+</script>
